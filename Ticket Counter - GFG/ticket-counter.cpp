3ef911c647333124855d08d3ed7c550fbcf89173
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2023-06-19 20:56:55 +0530

    Added solution - LeetHub

---
+++ Ticket Counter - GFG/ticket-counter.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int distributeTicket(int N, int K) {
+        int i=1,p=0;
+        while(i<N){
+            if(p){ N-=K;
+            p=0;
+            
+            
+            
+            }
+            else {i+=K;
+                p=1;
+            }
+        }
+        if(p){
+            return N;
+        }
+        return i;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        int K;
+        scanf("%d",&K);
+        
+        Solution obj;
+        int res = obj.distributeTicket(N, K);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
